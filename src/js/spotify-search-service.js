81835531b79e4b431637b2cef8036f6083583bdd
Benjamin McFarland <benrmcfarland@gmail.com>
2020-12-01 13:53:35 -0800

    Add spotify API code
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    co-authored-by: cory nordenbrock <cordenbrock@gmail.com>
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>

--- src/js/spotify-search-service.js
+++ src/js/spotify-search-service.js
@@ -1,9 +1,21 @@
-export default class Search {
-  static searchKeyword() {
-    return fetch (`URLURLURL`)
-      .then (function(response) {
+export default class Spotify {
+  static searchSpotify(keyword) {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer <ENTER HARD-CODED ACCESS TOKEN>");
+    myHeaders.append("Cookie", "_ga=GA1.2.456045822.1606765238; _gid=GA1.2.637614845.1606765238; sp_dc=AQB5RMpfdYpkmkJlRHPeYxSSfyTRPpkKySEvxhbkmixFgyFT0ZOlFeh5KY4xSVAXFpCp276qTTx4nbxD11fqn-6DnKhR9PuJ6yLMzkXUPg; sp_key=66b31764-29a3-4c2f-92c8-38ed42c7b352; _gat=1");
+
+    const requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    let type = "album";
+    let limit = 1;
+    return fetch(`https://api.spotify.com/v1/search?q=${keyword}&type=${type}&limit=${limit}`, requestOptions)
+      .then ((response) => {
         if (!response.ok) {
-          throw Error(response.statusplaceholder);
+          throw Error(response.statusText);
         }
         return response.json();
       })
--- src/main.js
+++ src/main.js
@@ -2,35 +2,29 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
+import Spotify from './../src/js/spotify.service.js';
 
 $(document).ready(function() {
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer [<ENTER ACCESS TOKEN>]);
-  myHeaders.append("Cookie", "_ga=GA1.2.456045822.1606765238; _gid=GA1.2.637614845.1606765238; sp_dc=AQB5RMpfdYpkmkJlRHPeYxSSfyTRPpkKySEvxhbkmixFgyFT0ZOlFeh5KY4xSVAXFpCp276qTTx4nbxD11fqn-6DnKhR9PuJ6yLMzkXUPg; sp_key=66b31764-29a3-4c2f-92c8-38ed42c7b352; _gat=1");
-
-  const requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
-
-  let type = "artist";
 
   $("#input").on("submit", function(e) {
     e.preventDefault();
-    let artist = $("#searchTerm").val();
-    fetch(`https://api.spotify.com/v1/search?q=${artist}&type=${type}`, requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+    let search = $("#searchTerm").val();
+
+    Spotify.searchSpotify(search)
+      .then(function(response) {
+        if (response instanceof Error) {
+          throw Error(`Spotify API error -- ${response.message}`);
+        }
+        let artistInfo, albumInfo, spotifyData;
+        artistInfo = response.albums.items[0].artists[0].name;
+        albumInfo = response.albums.items[0].name;
+        spotifyData = {artist: artistInfo, album: albumInfo};
+
+        console.log(spotifyData);
+        return spotifyData;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   });
 });
-
-// import SpotifyWebApi from 'spotify-web-api-js';
-// let spotify = new SpotifyWebApi();
-// spotify.setAccessToken('BQC2SS3L4MYZlqSy58ItenwwoDsV9Q66mXQ1zXP1n3q_oyXB1yNimy9XjmhmjNHJA2QuH5vfMsoUPX7p1aKBM33IbSPD5zL7JqXWHRxezapUcuOzHMJ4SG4X9eyjhTFZ4JpeTZCuWJOABKzF');
-// spotify.getArtistAlbums('alt-J', function (err, data) {
-//   if (err) console.error(err);
-//   else console.log('Artist albums', data);
-// });
