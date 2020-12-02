c555eca2d278d964e5215cb85ffe9b610e5df258
Benjamin McFarland <benrmcfarland@gmail.com>
2020-12-02 09:13:54 -0800

    Merge styling and playback features to main branch
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>
    co-authored-by: cory nordenbrock <cordenbrock@gmail.com>
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

--- src/main.js
+++ src/main.js
@@ -12,11 +12,32 @@ function discogsRequest(artist, album) {
   DiscogsService.getDiscogs(artist, album)
     .then(function(response) {
       console.log(response.results[1].master_id);
-      $("#output").html(`<a href="https://www.discogs.com/master/${response.results[1].master_id}">Click here</a>`)
+      $("#output").html(`<a href="https://www.discogs.com/master/${response.results[1].master_id}">Click here</a>`);
     });
 }
 
+function spotifyPlayback(albumID) {
+  $("#widget").html(`<iframe src="https://open.spotify.com/embed/album/${albumID}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`);
+}
+
 // User Interface Logic //
+
+$('#readMore').click (function() {
+  $('#intro').fadeOut();
+  $('#output').fadeIn();
+  $('#body').fadeIn();
+  $('#navbar').fadeIn();
+});
+$('#aboutButton').click (function() {
+  $('#body').hide();
+  $('#output').hide();
+  $('#about').show();
+});
+$('#homeButton').click (function() {
+  $('#output').show();
+  $('#body').show();
+  $('#about').hide();
+});
 
 $(document).ready(function() {
 
@@ -29,14 +50,18 @@ $(document).ready(function() {
         if (response instanceof Error) {
           throw Error(`Spotify API error -- ${response.message}`);
         }
-        let artistInfo, albumInfo, albumLink, albumCover, spotifyData;
+        let artistInfo, albumInfo, albumLink, albumCover, spotifyData, albumID;
         artistInfo = response.albums.items[0].artists[0].name;
         albumInfo = response.albums.items[0].name;
         albumLink = response.albums.items[0].external_urls.spotify;
         albumCover = response.albums.items[0].images[0].url;
-        spotifyData = {artist: artistInfo, album: albumInfo, link: albumLink, cover: albumCover};
+        albumID = response.albums.items[0].id;
+        spotifyData = {artist: artistInfo, album: albumInfo, link: albumLink, cover: albumCover, ID: albumID};
 
+        // Make Discogs request with album retrieved from Spotify (callback) // 
         discogsRequest(spotifyData.artist, spotifyData.album);
+        // Place Spotify widget into webpage with album retrieved from Spotify API (callback) //
+        spotifyPlayback(spotifyData.ID);
 
         console.log(spotifyData);
 
