367af0ef2833dcb735ab83b8f281597b2f172691
Benjamin McFarland <benrmcfarland@gmail.com>
2020-12-02 16:55:22 -0800

    Finish styling and searching functionality
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>
    co-authored-by: cory nordenbrock <cordenbrock@gmail.com>
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

--- src/js/spotify.service.js
+++ src/js/spotify.service.js
@@ -1,7 +1,7 @@
 export default class Spotify {
   static searchSpotify(keyword) {
     const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer BQDKJzr8bNIT4NaAq_Om9BSEM8QtFkBhyPqI4WIIDHxdzhVw7hzUV-dbWSwjOtO3DSI7gQU9ZMXsai-6BokiwB7F6PVjEqYJv6fv_lfCMmgsCUB3W2j2Pxnu4qnx__bm4OgorpV8Mrh_ZNWIM5uzVmK8AKPKQuPYqjE");
+    myHeaders.append("Authorization", "Bearer BQAFgfL0wozw_f6pdpFyiY1yWovIRBpAim2Os3q8T3HCtIPxobcRoBZCKtxlAM2rpmTiQedwZDiKOfUc-my00NBTUEqDw_mZJp1KbrOV1NDZj5VJh9MvIHWN5BPSN8BIiv8_3AvHV_3Nwkgy2TrILg6QyTqur5jg");
     myHeaders.append("Cookie", "_ga=GA1.2.456045822.1606765238; _gid=GA1.2.637614845.1606765238; sp_dc=AQB5RMpfdYpkmkJlRHPeYxSSfyTRPpkKySEvxhbkmixFgyFT0ZOlFeh5KY4xSVAXFpCp276qTTx4nbxD11fqn-6DnKhR9PuJ6yLMzkXUPg; sp_key=66b31764-29a3-4c2f-92c8-38ed42c7b352; _gat=1");
 
     const requestOptions = {
@@ -11,7 +11,7 @@ export default class Spotify {
     };
 
     let type = "album,track";
-    let limit = 1;
+    let limit = 50;
     return fetch(`https://api.spotify.com/v1/search?q=${keyword}&type=${type}&limit=${limit}`, requestOptions)
       .then ((response) => {
         if (!response.ok) {
--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import DiscogsService from './../src/js/discogs-services.js';
 
 // Business Logic //
 
+
+function printCard(obj) {
+  $("#cardMenu").prepend(`<div class="album-data card col-4"><img class="card-img-top" src="${obj.cover}" alt="Cover Art for the Album ${obj.album}"><ul class="list-group list-group-flush"><li class="list-group-item">${obj.artist}</li><li class="list-group-item">${obj.album}</li><li class="list-group-item" id="${obj.ID}"></li></ul></div>`);
+}
+
 function buildAlbumObj(obj) {
   let artistInfo, albumInfo, albumLink, albumCover, albumID, spotifyData;
   artistInfo = obj.artists[0].name;
@@ -15,13 +20,13 @@ function buildAlbumObj(obj) {
   albumLink = obj.external_urls.spotify;
   albumCover = obj.images[0].url;
   albumID = obj.id;
-  spotifyData = { artist: artistInfo, album: albumInfo, link: albumLink, cover: albumCover, ID: albumID };
+  spotifyData = { artist: artistInfo, album: albumInfo, link: albumLink, cover: albumCover, ID: albumID};
+
   return spotifyData;
 }
 
 function formatSearch(keyword, searchOption) {
   let newKeyword = keyword.replace(/\s\s+/g, "%20");
-  console.log(newKeyword);
 
   if (searchOption === "genre") {
     return ("%20genre:%22" + newKeyword + "%22");
@@ -34,18 +39,33 @@ function formatSearch(keyword, searchOption) {
   }
 }
 
-function discogsRequest(artist, album) {
+function discogsRequest(artist, album, id) {
   DiscogsService.getDiscogs(artist, album)
     .then(function(response) {
-      console.log(response.results[1].master_id);
-      $("#output").html(`<a href="https://www.discogs.com/master/${response.results[1].master_id}">Click here</a>`);
+      if (response.results.length > 0) {
+        $("#vinylLink").html(`<h4>Purchase Album:</h4> <a href="https://www.discogs.com/master/${response.results[1].master_id}">Available on Discogs</a>`);
+        $("#" + id).html(`<a href="https://www.discogs.com/master/${response.results[1].master_id}">Available on Discogs</a>`);
+      } else {
+        $("#vinylLink").html("<h5>Album not on Discogs</h5>");
+        $("#" + id).html("Album not on Discogs");
+      }
     });
 }
 
 function displayInfo(albumObj) {
-  $(".card").prepend(`<img class="card-img-top" src="${albumObj.cover}" alt="Cover Art for the Album ${albumObj.album}">`);
-  discogsRequest(albumObj.artist, albumObj.album);
-  $("#widget").html(`<iframe src="https://open.spotify.com/embed/album/${albumObj.ID}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`);
+  $("#featImage").html(`<img class="card-img-top" id="heroImage" src="${albumObj.cover}" alt="Cover Art for the Album ${albumObj.album}">`);
+  $("#featWidget").html(`<iframe src="https://open.spotify.com/embed/album/${albumObj.ID}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`);
+  $("#featArtist").html(`<h4>Artist Name:</h4> ${albumObj.artist}`);
+  $("#featAlbum").html(`<h4>Album Name:</h4> ${albumObj.album}`);
+  $("#featYear").html(`<h4>Release Year:</h4> ${albumObj.album}`);
+  discogsRequest(albumObj.artist, albumObj.album, albumObj.ID);
+}
+
+function printNoResult() {
+  $("#featImage").html(`<h2>No Result Found...</h2>`);
+  $("#featWidget").html(" ");
+  $("#featArtist").html(" ");
+  $("#featAlbum").html(" ");
 }
 
 // User Interface Logic //
@@ -91,17 +111,21 @@ $(document).ready(function() {
           throw Error(`Spotify API error -- ${response.message}`);
         }
         if (searchOption != "genre") {
-          console.log(response);
-          let albumsObj = response.albums.items[0];
+          let randomNumAlbum = Math.floor(Math.random() * (response.albums.items.length));
+          let albumsObj = response.albums.items[randomNumAlbum];
           let album = buildAlbumObj(albumsObj);
-          console.log(album);
+          printCard(album);
           displayInfo(album);
         } else {
-          console.log(response);
-          let tracksObj = response.tracks.items[0].album;
-          let album = buildAlbumObj(tracksObj);
-          console.log(album);
-          displayInfo(album);
+          if (response.tracks.items.length > 0) {
+            let randomNumTrack = Math.floor(Math.random() * (response.tracks.items.length));
+            let tracksObj = response.tracks.items[randomNumTrack].album;
+            let album = buildAlbumObj(tracksObj);
+            printCard(album);
+            displayInfo(album);
+          } else {
+            printNoResult();
+          }
         }
       })
       .catch(function(err) {
